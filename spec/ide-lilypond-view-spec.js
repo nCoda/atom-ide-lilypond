cc894662063cdd6be624d70a9088fac8ca6719cc
Christopher Antila <christopher@antila.ca>
2018-07-11 21:53:21 -0400

    Initial commit

---
+++ spec/ide-lilypond-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IdeLilypondView from '../lib/ide-lilypond-view';
+
+describe('IdeLilypondView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
